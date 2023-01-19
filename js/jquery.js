f0910e1286c27b70fca6b618cadb3f9e24582329
Abdul Hadi <abdul.806@yahoo.com>
2023-01-19 12:16:36 +0500

    first commit

---
+++ js/jquery.js
@@ -0,0 +1,11 @@
+$(".arrow").click(function () {
+    $('html, body').animate({
+        scrollTop: $(".schedule-inner").offset().top
+    }, 800);
+});
+//contact-us message
+$('html, body').ready(function () {
+    $(".testimonial-button").click(function () {
+        alert("Page not avaliable");
+    });
+});
